82bd644296f84b9a6577215ccde03ef2ecffb7c5
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-09-17 19:03:30 -0300

    Resolved Conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './style.css';
 
 const App = () => {
+
+  const [num, setNum] = useState(0);
+  const [timer, setTimer] = useState(null);
+  const [actionBtn, setactionBtn] = useState('Vai')
+
+  const vai = () => {
+    if (timer !== null) {
+      clearInterval(timer);
+      setTimer(null);
+      setactionBtn('Vai')
+    } else {
+      setTimer(setInterval(() => {
+        setNum(num => num + 0.1);
+      }, 100));
+      setactionBtn('Parar')
+    }
+
+  }
+
+  const limpar = () => {
+    clearInterval(timer);
+    setTimer(null);
+    setNum(num => 0)
+    setactionBtn('Vai')
+  }
+
+
+
   return (
-    <div>
-      oiii
+    <div className="container">
+      <img src={require('./assets/cronometro.png')} className="img" />
+      <a className="timer">{num.toFixed(1)}</a>
+      <div className="areaBtn">
+        <a className="botao" onClick={vai}>{actionBtn}</a>
+        <a className="botao" onClick={limpar}>Limpar</a>
+      </div>
     </div>
   )
 }
